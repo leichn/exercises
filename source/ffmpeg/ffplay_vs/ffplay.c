014b855cf10b9250e18a0ff66b5c2a9a6ed69f43
leisure <lei.huang.chn@gmail.com>
2019-01-07 22:03:05 +0800

    更新

--- source/ffmpeg/ffplay_vs/ffplay.c
+++ source/ffmpeg/ffplay_vs/ffplay.c
@@ -117,11 +117,11 @@ typedef struct MyAVPacketList {
 
 typedef struct PacketQueue {
     MyAVPacketList *first_pkt, *last_pkt;
-    int nb_packets;     // 队列中packet的数量
-    int size;           // 队列所占内存空间大小
-    int64_t duration;   // 队列中所有packet总的播放时长
+    int nb_packets;                 // 队列中packet的数量
+    int size;                       // 队列所占内存空间大小
+    int64_t duration;               // 队列中所有packet总的播放时长
     int abort_request;
-    int serial;         // 播放序列，所谓播放序列就是一段连续的播放动作，一个seek操作会启动一段新的播放序列
+    int serial;                     // 播放序列，所谓播放序列就是一段连续的播放动作，一个seek操作会启动一段新的播放序列
     SDL_mutex *mutex;
     SDL_cond *cond;
 } PacketQueue;
